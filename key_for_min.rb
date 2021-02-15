8872e8d2f4f978e3b04cdfa9733fa17791723c5d
Devante Lowery <dlowery2120@gmail.com>
2021-02-15 02:37:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_value = ""
+  name_hash.each do |name, value|
+    if name_hash.min 
+      puts lowest_value << name_hash[:name]
+    end
+  end
 end
